bb04f099ea3b0db1fd3a73d925740c0974a5b4e7
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-01 10:51:52 -0500

    Getting the user creation working on the backend. Using dummy views.

---
+++ poolie_app/app/helpers/sessions_helper.rb
@@ -0,0 +1,19 @@
+module SessionsHelper
+    def log_in(user)
+        session[:id] = user.id
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:id])
+    end
+
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def log_out
+        session.destroy
+        @current_user = nil
+    end
+
+end
