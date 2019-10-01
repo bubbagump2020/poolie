024b039d6f7ce3eca4cdd8864b65cfbdd85c57c5
github username <github email address>
2019-10-01 15:12:15 -0500

    basic collision

--- poolie_app/poolie_frontend/assets/js/Ball.js
+++ poolie_app/poolie_frontend/assets/js/Ball.js
@@ -1,20 +1,73 @@
 const BALL_ORIGIN = new Vector2(25, 25);
+const BALL_DIAMETER = 38;
 
-function Ball(position) {
+function Ball(position, color) {
   this.position = position;
   this.velocity = new Vector2();
+  this.moving = false;
+  this.sprite = getBallSpriteByColor(color);
 }
 
 Ball.prototype.update = function(delta) {
-  this.position.addTo(this.velocity);
-//   this.velocity = this.velocity.mult(0.98);
+  this.position.addTo(this.velocity.mult(delta));
+  this.velocity = this.velocity.mult(0.98);
+  if (this.velocity.length() < 5) {
+    this.velocity = new Vector2();
+    this.moving = false;
+  }
 };
 
 Ball.prototype.draw = function() {
-  Canvas.drawImage(sprites.cueBall, this.position, BALL_ORIGIN);
+  Canvas.drawImage(this.sprite, this.position, BALL_ORIGIN);
 };
 
 Ball.prototype.shoot = function(power, rotation) {
-  console.log("shoot your shot friend");
-  this.velocity = new Vector2(Math.cos(rotation), Math.sin(rotation));
+  this.velocity = new Vector2(
+    power * Math.cos(rotation),
+    power * Math.sin(rotation)
+  );
+  this.moving = true;
+};
+
+Ball.prototype.collideWith = function(ball) {
+  // finding a normalish vector -- I'm no mathematician
+  const normalVector = this.position.subtract(ball.position);
+//   console.log("normal vectors!")
+
+  // find distance
+  const dist = normalVector.length();
+
+  if (dist > BALL_DIAMETER) {
+    return;
+  }
+
+  // finding "unit normal vector"
+  const unitNormal = normalVector.mult(1 / normalVector.length());
+
+  // finding "unit tangent vector"
+  const unitTangent = new Vector2(-unitNormal.y, unitNormal.x);
+
+  // apply velocities to unit normal and unit tangent vectors
+  const velocityOneNormal = unitNormal.dot(this.velocity);
+  const velocityOneTangent = unitTangent.dot(this.velocity);
+  const velocityTwoNormal = unitNormal.dot(ball.velocity);
+  const velocityTwoTangent = unitTangent.dot(ball.velocity);
+
+  // determine the new normal velocities
+  let velocityOneNormalTag = velocityTwoNormal;
+  let velocityTwoNormalTag = velocityOneNormal;
+
+  // convert scalar normal and tangential velocities into vectors
+
+  velocityOneNormalTag = unitNormal.mult(velocityOneNormalTag);
+  const velocityOneTangentTag = unitTangent.mult(velocityOneTangent);
+  velocityTwoNormalTag = unitNormal.mult(velocityTwoNormalTag);
+  const velocityTwoTangentTag = unitTangent.mult(velocityTwoTangent);
+
+  // update velocities
+  this.velocity = velocityOneNormalTag.add(velocityOneTangentTag);
+  ball.velocity = velocityTwoNormalTag.add(velocityTwoTangentTag);
+
+  this.moving = true;
+  ball.moving = true;
 };
--- poolie_app/poolie_frontend/assets/js/Stick.js
+++ poolie_app/poolie_frontend/assets/js/Stick.js
@@ -7,39 +7,44 @@ function Stick(position, onShoot) {
   this.origin = STICK_ORIGIN.copy();
   this.power = 0;
   this.onShoot = onShoot;
+  this.shot = false;
 }
 
 Stick.prototype.update = function() {
+  if (Mouse.left.down) {
+    this.increasePower();
+  } else if (this.power > 0) {
+    this.shoot();
+  }
 
-    if(Mouse.left.down){
-        this.increasePower();
-    } else if(this.power > 0) {
-        this.shoot()
-    }
-
-    this.updateRotation();
-
+  this.updateRotation();
 };
 
 Stick.prototype.draw = function() {
   Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation);
-
 };
 
-Stick.prototype.updateRotation = function(){
-    let opposite = Mouse.position.y - this.position.y;
-    let adjacent = Mouse.position.x - this.position.x;
+Stick.prototype.updateRotation = function() {
+  let opposite = Mouse.position.y - this.position.y;
+  let adjacent = Mouse.position.x - this.position.x;
 
-    this.rotation = Math.atan2(opposite, adjacent);
-}
+  this.rotation = Math.atan2(opposite, adjacent);
+};
 
-Stick.prototype.increasePower = function(){
-    this.power += 100;
-    this.origin.x += 5;
-}
+Stick.prototype.increasePower = function() {
+  this.power += 100;
+  this.origin.x += 5;
+};
 
-Stick.prototype.shoot = function(){
-    this.onShoot(this.power, this.rotation);
-    this.power = 0;
-    this.origin = STICK_SHOT_ORIGIN.copy();
-}
+Stick.prototype.shoot = function() {
+  this.onShoot(this.power, this.rotation);
+  this.power = 0;
+  this.origin = STICK_SHOT_ORIGIN.copy();
+  this.shot = true;
+};
+
+Stick.prototype.reposition = function(position) {
+  this.position = position.copy();
+  this.origin = STICK_ORIGIN.copy();
+  this.shot = false;
+};
