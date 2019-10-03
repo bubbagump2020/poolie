06faf7b9ecab0e6796309fe7c72f51f38f1aa571
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-03 10:29:22 -0500

    Working chat channels, not quite realtime yet.

--- poolie_app/app/models/chat.rb
+++
@@ -1,4 +0,0 @@
-class Chat < ApplicationRecord
-	has_man :chat_messages, dependent: :destroy,
-							inverse_of: :room
-end
--- poolie_app/app/models/user.rb
+++ poolie_app/app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
-	validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+
+  def gravatar_url
+    gravatar_id = Digest::MD5::hexdigest(email).downcase
+    "https://gravatar.com/avatar/#{gravatar_id}.png"
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+
+
 end
